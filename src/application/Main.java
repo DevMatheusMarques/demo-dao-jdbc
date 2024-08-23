42c00e31e67d66393379e869f799766ee559250b
DevMatheusMarques <estudosfatecmhcm@gmail.com>
2024-08-23 15:52:39 -0300

    feat: SellerDaoJDBC and DaoFactory

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,6 +4,8 @@ import com.mysql.cj.jdbc.Driver;
 import db.DB;
 import db.DbException;
 import db.DbIntegrityException;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -21,6 +23,8 @@ public class Main {
         Department department = new Department(1, "RH");
 
         Seller seller = new Seller(1, "Matheus", "matheus@gmail.com", new Date(), 3000.0, department);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(department);
         System.out.println(seller);
